112cc4d98f6eccade21daeb992f0db439ed3909e
Rolivhuwa-Thomoli <rolivhuwathomoli@gmail.com>
2025-04-24 14:30:24 +0200

    Update Main 2025/04/24 14:29
    Added and corrected comments

--- array/src/Main.java
+++ array/src/Main.java
@@ -1,7 +1,8 @@
 //******IMPORTANT NOTES*************
-//Objective:   >Enter specified number of students' details in an array of type Student.
-//             >Also handle exception for mismatched input
-//             >Allow the user another chance to enter the correct input during runtime.
+//Objective:   > Prompt the user to Enter number of students.
+//             > store their details(name and grade) in an array of type Student.
+//             > Also handle exception for mismatched input
+//             > Allow the user another chance to enter the correct input during runtime.
 //
 //Challenges:  > Not clearing the invalid input causes skips and infinite loops
 //             > InputMismatchException not included in the available libraries.
@@ -9,10 +10,11 @@
 //
 //resolutions: > Import all .util classes to include the InputMismatchedException classes
 //             > use a do while to repeat prompts
-//             > use a try-catch InputMismatchedException e to catch wrong input.
-//             > clear the input after nextInt(), both try and catch statements.
-//             > initialize dummy values on all user input variables on the catch statement e.g. numOfStudents = 0;
 //             > for do-while initialize validInput as false, set to true at end of try, loop while validInput is false.
+//             > use a try-catch InputMismatchedException e to catch wrong input.
+//             > clear the input after nextInt(), both try and catch statements to avoid skips and loops.
+//             > initialize dummy values on all user input variables on the catch statement e.g. numOfStudents = 0;
+
 
 import java.util.*;
 
@@ -57,12 +59,12 @@ public class Main {
             try {
                 System.out.println("Enter the number of students");
                 numberOfStudents = sc.nextInt();
-                sc.nextLine();//clear the '\n' character
-                validInput = true;
+                sc.nextLine();//clear the '\n' character at the end of input
+                validInput = true;// since try was successful.
             }catch (InputMismatchException e){
                 System.out.println("Please enter a valid number");
                 System.out.println();
-                numberOfStudents = 0;
+                numberOfStudents = 0;//resolve uninitialized error
                 sc.nextLine();//clear the input
             }
         }while(!validInput);// ATTENTION!!! remember to end with semicolon PLEASE
@@ -81,8 +83,8 @@ public class Main {
                     System.out.println("Invalid input");
                     System.out.println();
                     sc.nextLine(); //Attention!! clear input for next while loop
-                    studentGrade = 0;
-                    studentName = "";
+                    studentGrade = 0;//resolve uninitialized error
+                    studentName = "";//resolve uninitialized error
                     validInput = false;
                 }
 
